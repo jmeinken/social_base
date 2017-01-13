205eace95fa20975910b56a8c4854111512560e9
jmeinken <thebellsofohio@hotmail.com>
2017-01-13 14:17:03 -0500

    removed .po and .mo files

--- social_base/urls.py
+++ social_base/urls.py
@@ -18,10 +18,15 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^rosetta/', include('rosetta.urls')),
+    
     url(r'^images/', include('images.urls', namespace="images")),
     url(r'^email_handler/', include('email_handler.urls', namespace="email_handler")),
     url(r'^microfeed2/', include('microfeed2.urls', namespace="microfeed2")),
     url(r'^pages/', include('pages.urls', namespace="pages")),
     url(r'^field_trans/', include('field_trans.urls', namespace="field_trans")),
     url(r'^', include('main.urls')),
+    
 ]
+
+
